d8729f047809f0fffb17fa2bf70acf0e371410f8
theevil <948861433@qq.com>
2022-04-30 16:28:06 +0800

    change GRU.py

--- GRU.py
+++ GRU.py
@@ -6,26 +6,21 @@ import torch
 import torch.nn as nn
 
 class GRUModel(nn.Module):
-    def __init__(self, input_num, hidden_num, output_num):
+    def __init__(self, input_num, output_num):
         super(GRUModel, self).__init__()
-        self.hidden_size = hidden_num
-        self.grucell = nn.GRUCell(input_num, hidden_num)
-        self.out_linear = nn.Linear(hidden_num, output_num)
+        self.hidden_size = output_num
+        self.grucell = nn.GRUCell(input_num, output_num)
 
     def forward(self, x, hid):
         # x : seq_lenth * batch * embed_size(=input_size)
-        # hid : seq_lenth * batch * embed_size(=hidden_size)
-        # return :[
-        #
-        # ]
+        # hid : batch * embed_size(=hidden_size)
+        output = []
         if hid is None:
-            hid = torch.randn(x.shape[0], x.shape[1], self.hidden_size)
-        y = torch.zeros(x.shape[0], x.shape[1], self.hidden_size)
+            hid = torch.randn(x.shape[0], self.hidden_size)
         for i in range(x.shape[0]):
-            #
-            y[i] = self.grucell(x[i], hid[i])
-            # print("y[i]: {}".format(y[i].shape))
+            hx = self.grucell(x[i], hid)
+            output.append(hx)
+        # print(output)
+        output = torch.stack(output)
 
-        # print("y: {}".format(y.shape))
-
-        return y, hid.detach()
+        return output, hx
